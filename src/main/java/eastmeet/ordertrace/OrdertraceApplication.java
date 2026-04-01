8b2e9ec3e5bd7650a723d689c579583341d68152
이동우 <ys932184@gmail.com>
2026-04-01 09:58:34 +0900

    chore: 초기 환경 구성 세팅

---
+++ src/main/java/eastmeet/ordertrace/OrdertraceApplication.java
@@ -0,0 +1,13 @@
+package eastmeet.ordertrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdertraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdertraceApplication.class, args);
+    }
+
+}
